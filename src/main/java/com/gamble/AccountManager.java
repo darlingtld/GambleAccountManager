b7e65d68f162d7a8ff655b6aace7e0ac418385bc
lingda <lingda@splunk.com>
2017-10-18 11:05:40 +0800

    replace console

--- src/main/java/com/gamble/AccountManager.java
+++ src/main/java/com/gamble/AccountManager.java
@@ -19,19 +19,19 @@ public class AccountManager {
         String path = "/Users/lingda/Downloads/bjracingcar";
         Integer expireYear = 2017;
         Integer expireMonth = 11;
-        Integer expireDay = 5;
-        String bossEmail = "";
+        Integer expireDay = 18;
+        String bossEmail = "abc@ef.com";
+
+        String branch = "topspeed"; // cloudapex
+        String version = "极速";
+        String bjracingcar = "BJRacingCar";
+        String user="rita1";
 
         Map<String, String> accountMap = new HashMap<>();
-//        accountMap.put("dear001", "master");
-//        accountMap.put("dd9902", "135-6810");
-//        accountMap.put("dd9903", "147-258");
-//        accountMap.put("dd9905", "147-369");
-        accountMap.put("dd9901", ModeEnum.MODE_135_6810.getMode());
-        accountMap.put("dd9902", ModeEnum.MODE_246_579.getMode());
-        accountMap.put("dd9903", ModeEnum.BASIC.getMode());
-        accountMap.put("dd9905", ModeEnum.MODE_147_369.getMode());
-        accountMap.put("dd9906", ModeEnum.BASIC.getMode());
+        accountMap.put("d36986", ModeEnum.MASTER.getMode());
+        accountMap.put("d36987", ModeEnum.MASTER.getMode());
+        accountMap.put("d36998", ModeEnum.MASTER.getMode());
+        accountMap.put("d36999", ModeEnum.MASTER.getMode());
 
         List<String> dirList = new ArrayList<>();
 
@@ -39,8 +39,8 @@ public class AccountManager {
             String accountDir = createDirectory(path, account.getKey(), account.getValue());
             dirList.add(accountDir.substring(accountDir.lastIndexOf("/") + 1));
 //          bjracingcar
-            System.out.println("[git clone https://github.com/darlingtld/BeijingRacingCar.git] in " + accountDir);
-            Process process = Runtime.getRuntime().exec("git clone -b cloudapex https://github.com/darlingtld/BeijingRacingCar.git", null, new File(accountDir));
+            System.out.println("[git clone -b " + branch + " https://gitee.com/darlingtld/BJRacingCar.git] in " + accountDir);
+            Process process = Runtime.getRuntime().exec("git clone -b " + branch + " https://gitee.com/darlingtld/BJRacingCar.git", null, new File(accountDir));
             process.waitFor();
 
             System.out.println("[rewrite application.properties] ");
@@ -54,39 +54,39 @@ public class AccountManager {
             replaceMap.put("gamble.bet.mimic", "false");
             replaceMap.put("server.port", "26000");
             replaceMap.put("spring.data.mongodb.port", "27000");
-            rewriteApplicationProperties(String.valueOf(Paths.get(accountDir, "BeijingRacingCar", "src", "main", "resources", "application.properties")), replaceMap);
+            rewriteApplicationProperties(String.valueOf(Paths.get(accountDir, bjracingcar, "src", "main", "resources", "application.properties")), replaceMap);
 
-            System.out.println("[gradle build -x test] in " + String.valueOf(Paths.get(accountDir, "BeijingRacingCar")));
-            Process process1 = Runtime.getRuntime().exec("gradle build -x test", null, new File(String.valueOf(Paths.get(accountDir, "BeijingRacingCar"))));
+            System.out.println("[gradle build -x test] in " + String.valueOf(Paths.get(accountDir, bjracingcar)));
+            Process process1 = Runtime.getRuntime().exec("gradle build -x test", null, new File(String.valueOf(Paths.get(accountDir, bjracingcar))));
             process1.waitFor();
 
-            System.out.println("[cp build/libs/BeijingRacingCar-0.0.1-SNAPSHOT.jar ../] in " + String.valueOf(Paths.get(accountDir, "BeijingRacingCar")));
-            Process process2 = Runtime.getRuntime().exec("cp build/libs/BeijingRacingCar-0.0.1-SNAPSHOT.jar ../", null, new File(String.valueOf(Paths.get(accountDir, "BeijingRacingCar"))));
+            System.out.println("[cp build/libs/BJRacingCar-0.0.1-SNAPSHOT.jar ../] in " + String.valueOf(Paths.get(accountDir, bjracingcar)));
+            Process process2 = Runtime.getRuntime().exec("cp build/libs/BJRacingCar-0.0.1-SNAPSHOT.jar ../", null, new File(String.valueOf(Paths.get(accountDir, bjracingcar))));
             process2.waitFor();
 
-            System.out.println("[cp /Users/lingda/IdeaProjects/GambleAccountManager/src/main/resources/" + account.getValue() + "/GambleConsole.jar ../] in " + String.valueOf(Paths.get(accountDir, "BeijingRacingCar")));
-            Process process3 = Runtime.getRuntime().exec("cp /Users/lingda/IdeaProjects/GambleAccountManager/src/main/resources/" + account.getValue() + "/GambleConsole.jar ../", null, new File(String.valueOf(Paths.get(accountDir, "BeijingRacingCar"))));
+            System.out.println("[cp /Users/lingda/IdeaProjects/GambleAccountManager/src/main/resources/" + account.getValue() + "/GambleConsole.jar ../] in " + String.valueOf(Paths.get(accountDir, bjracingcar)));
+            Process process3 = Runtime.getRuntime().exec("cp /Users/lingda/IdeaProjects/GambleAccountManager/src/main/resources/" + account.getValue() + "/GambleConsole.jar ../", null, new File(String.valueOf(Paths.get(accountDir, bjracingcar))));
             process3.waitFor();
 
-            System.out.println("[cp /Users/lingda/IdeaProjects/GambleAccountManager/src/main/resources/chromedriver.exe ../] in " + String.valueOf(Paths.get(accountDir, "BeijingRacingCar")));
-            Process process4 = Runtime.getRuntime().exec("cp /Users/lingda/IdeaProjects/GambleAccountManager/src/main/resources/chromedriver.exe ../", null, new File(String.valueOf(Paths.get(accountDir, "BeijingRacingCar"))));
+            System.out.println("[cp /Users/lingda/IdeaProjects/GambleAccountManager/src/main/resources/chromedriver.exe ../] in " + String.valueOf(Paths.get(accountDir, bjracingcar)));
+            Process process4 = Runtime.getRuntime().exec("cp /Users/lingda/IdeaProjects/GambleAccountManager/src/main/resources/chromedriver.exe ../", null, new File(String.valueOf(Paths.get(accountDir, bjracingcar))));
             process4.waitFor();
 
-            System.out.println("[write start_1_db] in " + String.valueOf(Paths.get(accountDir, "BeijingRacingCar")));
+            System.out.println("[write start_1_db] in " + String.valueOf(Paths.get(accountDir, bjracingcar)));
             writeStartDb(accountDir);
 
-            System.out.println("[write start_2_app] in " + String.valueOf(Paths.get(accountDir, "BeijingRacingCar")));
+            System.out.println("[write start_2_app] in " + String.valueOf(Paths.get(accountDir, bjracingcar)));
             writeStartApp(accountDir);
 
-            System.out.println("[write start_3_console] in " + String.valueOf(Paths.get(accountDir, "BeijingRacingCar")));
+            System.out.println("[write start_3_console] in " + String.valueOf(Paths.get(accountDir, bjracingcar)));
             writeStartConsole(accountDir);
 
-            System.out.println("[delete BeijingRacingCar source] in " + String.valueOf(Paths.get(accountDir, "BeijingRacingCar")));
-            deleteDirectory(new File(String.valueOf(Paths.get(accountDir, "BeijingRacingCar"))));
+            System.out.println("[delete BeijingRacingCar source] in " + String.valueOf(Paths.get(accountDir, bjracingcar)));
+            deleteDirectory(new File(String.valueOf(Paths.get(accountDir, bjracingcar))));
 
 
         }
-        String zipCommand = "zip -r " + String.format("%s_%s.zip ", String.join("-", accountMap.entrySet().stream().map(Map.Entry::getKey).collect(Collectors.toList())), String.format("%04d%02d%02d", expireYear, expireMonth, expireDay) + "到期") + String.join(" ", dirList);
+        String zipCommand = "zip -r " + String.format("%s_%s_%s.zip ", version, String.join("-", accountMap.entrySet().stream().map(Map.Entry::getKey).collect(Collectors.toList())), String.format("%04d%02d%02d", expireYear, expireMonth, expireDay) + "到期") + String.join(" ", dirList);
         System.out.println("[create zip file] " + zipCommand);
         Process process5 = Runtime.getRuntime().exec(zipCommand, null, new File(path));
         process5.waitFor();
@@ -101,7 +101,7 @@ public class AccountManager {
     }
 
     private static void writeStartApp(String accountDir) throws IOException {
-        String line = "\"C:\\Program Files\\Java\\jdk1.8.0_144\\bin.\\java.exe\" -jar -Djava.net.preferIPv4Stack=true \"C:\\Users\\USER\\Downloads\\BeijingRacingCar-0.0.1-SNAPSHOT.jar\" C:\\Users\\USER\\Downloads\\chromedriver.exe";
+        String line = "\"C:\\Program Files\\Java\\jdk1.8.0_144\\bin.\\java.exe\" -jar -Djava.net.preferIPv4Stack=true \"C:\\Users\\USER\\Downloads\\BJRacingCar-0.0.1-SNAPSHOT.jar\" C:\\Users\\USER\\Downloads\\chromedriver.exe";
         BufferedWriter bw = new BufferedWriter(new FileWriter(String.valueOf(Paths.get(accountDir, "start_2_app.bat"))));
         bw.write(line);
         bw.flush();
